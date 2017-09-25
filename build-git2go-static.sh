485b42f7a6c716118db3ce753b4a6e5815ad5d09
buddyspike <buddyspike@geeksdiary.com>
2017-09-26 05:19:07 +1000

    ignore submodule update errors.

--- build-git2go-static.sh
+++ build-git2go-static.sh
@@ -5,11 +5,11 @@ set -ex
 GIT2GO_PATH=$GOPATH/src/github.com/libgit2/git2go
 GIT2GO_VENDOR_PATH=$GIT2GO_PATH/vendor/libgit2
 
-go get github.com/libgit2/git2go &&
+go get github.com/libgit2/git2go || true &&
 
 pushd $GIT2GO_PATH &&
 git checkout v26 &&
-git submodule update --init &&
+git submodule update --init || true &&
 
 pushd $GIT2GO_VENDOR_PATH &&
 mkdir -p install/lib &&
