11e665a9af0da8dd48968930becc1204af96ae0d
buddyspike <buddyspike@geeksdiary.com>
2017-09-26 05:22:43 +1000

    removed pushd calls.

--- build-git2go-static.sh
+++ build-git2go-static.sh
@@ -2,19 +2,20 @@
 
 set -ex
 
+DIR=$(pwd)
 GIT2GO_PATH=$GOPATH/src/github.com/libgit2/git2go
 GIT2GO_VENDOR_PATH=$GIT2GO_PATH/vendor/libgit2
 
 go get github.com/libgit2/git2go || true &&
 
-pushd $GIT2GO_PATH &&
+cd $GIT2GO_PATH &&
 git checkout v26 &&
 git submodule update --init || true &&
 
-pushd $GIT2GO_VENDOR_PATH &&
+cd $GIT2GO_VENDOR_PATH &&
 mkdir -p install/lib &&
 mkdir -p build &&
-pushd build &&
+cd build &&
 cmake -DTHREADSAFE=ON \
       -DBUILD_CLAR=OFF \
       -DBUILD_SHARED_LIBS=OFF \
@@ -28,4 +29,4 @@ make -j2 install &&
 
 go install github.com/libgit2/git2go &&
 
-popd && popd && popd 
+cd $DIR
