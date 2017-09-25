0744ecb5f40d220c0c4a1556b89e3bd351070b17
buddyspike <buddyspike@geeksdiary.com>
2017-09-26 04:53:16 +1000

    build dependencies.

---
+++ build-git2go-static.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -ex
+
+GIT2GO_PATH=$GOPATH/src/github.com/libgit2/git2go
+GIT2GO_VENDOR_PATH="$GIT2GO_PATH/vendor/libgit2"
+
+pushd $GIT2GO_VENDOR_PATH &&
+mkdir -p install/lib &&
+mkdir -p build &&
+pushd build &&
+cmake -DTHREADSAFE=ON \
+      -DBUILD_CLAR=OFF \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_C_FLAGS=-fPIC \
+      -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+      -DCMAKE_INSTALL_PREFIX=../install \
+      .. &&
+
+cmake --build . &&
+make -j2 install &&
+
+go install github.com/libgit2/git2go &&
+
+popd && popd
