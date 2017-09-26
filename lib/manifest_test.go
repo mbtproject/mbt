356691f3a02e4e2cb23d206c31838ab57ab5e55e
buddyspike <buddyspike@geeksdiary.com>
2017-09-27 08:36:38 +1000

    tests++

--- lib/manifest_test.go
+++ lib/manifest_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestApplicationDetection(t *testing.T) {
+func TestSingleAppDir(t *testing.T) {
 	clean()
 	// defer clean()
 
@@ -26,7 +26,8 @@ func TestApplicationDetection(t *testing.T) {
 	assert.Equal(t, "app-a", m.Applications[0].Name)
 }
 
-func TestNonApplicationDirectories(t *testing.T) {
+func TestNonAppContent(t *testing.T) {
+	clean()
 	repo, err := createTestRepository(".tmp/repo")
 	assert.NoError(t, err)
 
@@ -43,4 +44,38 @@ func TestNonApplicationDirectories(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Len(t, m.Applications, 1)
+}
+
+func TestNestedAppDir(t *testing.T) {
+	clean()
+	repo, err := createTestRepository(".tmp/repo")
+	assert.NoError(t, err)
+
+	assert.NoError(t, repo.InitApplication("a/b/c/app-a"))
+	assert.NoError(t, repo.Commit("first"))
+
+	m, err := fromBranch(repo.Repo, ".tmp/repo", "master")
+	assert.NoError(t, err)
+
+	assert.Len(t, m.Applications, 1)
+	assert.Equal(t, "a/b/c/app-a", m.Applications[0].Path)
+}
+
+func TestAppsDirInAppDir(t *testing.T) {
+	clean()
+	repo, err := createTestRepository(".tmp/repo")
+	assert.NoError(t, err)
+
+	assert.NoError(t, repo.InitApplication("app-a"))
+	assert.NoError(t, repo.InitApplication("app-a/app-b"))
+	assert.NoError(t, repo.Commit("first"))
+
+	m, err := fromBranch(repo.Repo, ".tmp/repo", "master")
+	assert.NoError(t, err)
+
+	assert.Len(t, m.Applications, 2)
+	assert.Equal(t, "app-b", m.Applications[0].Name)
+	assert.Equal(t, "app-a/app-b", m.Applications[0].Path)
+	assert.Equal(t, "app-a", m.Applications[1].Name)
+	assert.Equal(t, "app-a", m.Applications[1].Path)
 }
