27cd75770ebdca989182a1ce493b27bcdb39cb40
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-10-15 15:22:28 +1100

    Added commentry to dicovery logic

--- lib/discover.go
+++ lib/discover.go
@@ -20,6 +20,14 @@ type applicationMetadata struct {
 	spec *Spec
 }
 
+func newApplicationMetadata(dir string, hash string, spec *Spec) *applicationMetadata {
+	return &applicationMetadata{
+		dir:  dir,
+		hash: hash,
+		spec: spec,
+	}
+}
+
 // BuildCmd represents the structure of build configuration in .mbt.yml.
 type BuildCmd struct {
 	Cmd  string
@@ -32,22 +40,6 @@ type Spec struct {
 	Build        map[string]*BuildCmd   `yaml:"build"`
 	Properties   map[string]interface{} `yaml:"properties"`
 	Dependencies []string               `yaml:"dependencies"`
-}
-
-// applicationMetadataSet is an array of ApplicationMetadata extracted from the repository.
-type applicationMetadataSet []*applicationMetadata
-
-type applicationMetadataNode struct {
-	metadata *applicationMetadata
-	set      map[string]*applicationMetadata
-}
-
-func newApplicationMetadata(dir string, hash string, spec *Spec) *applicationMetadata {
-	return &applicationMetadata{
-		dir:  dir,
-		hash: hash,
-		spec: spec,
-	}
 }
 
 func newSpec(content []byte) (*Spec, error) {
@@ -64,31 +56,15 @@ func newSpec(content []byte) (*Spec, error) {
 	return a, nil
 }
 
-func newApplicationMetadataNode(metadata *applicationMetadata, set map[string]*applicationMetadata) *applicationMetadataNode {
-	return &applicationMetadataNode{metadata, set}
-}
+// applicationMetadataSet is an array of ApplicationMetadata extracted from the repository.
+type applicationMetadataSet []*applicationMetadata
 
-func (n *applicationMetadataNode) GetID() interface{} {
-	return n.metadata.spec.Name
-}
-
-func (n *applicationMetadataNode) GetChildren() ([]graph.Node, error) {
-	c := []graph.Node{}
-
-	for _, d := range n.metadata.spec.Dependencies {
-		if s, ok := n.set[d]; ok {
-			c = append(c, newApplicationMetadataNode(s, n.set))
-		} else {
-			return nil, fmt.Errorf("dependency not found %s -> %s", n.metadata.spec.Name, d)
-		}
-	}
-
-	return c, nil
-}
-
+// toApplications transforms an applicationMetadataSet to Applications structure
+// while establishing the dependency links.
 func (a applicationMetadataSet) toApplications(withDependencies bool) (Applications, error) {
 	m := make(map[string]*applicationMetadata)
 
+	// 1. Transform each applicationMetadatadata into applicationMetadataNode for sorting.
 	for _, meta := range a {
 		m[meta.spec.Name] = meta
 	}
@@ -98,11 +74,14 @@ func (a applicationMetadataSet) toApplications(withDependencies bool) (Applicati
 		nodes[i] = newApplicationMetadataNode(meta, m)
 	}
 
+	// 2. Sort
 	sortedNodes, err := graph.TopSort(nodes)
 	if err != nil {
 		return nil, err
 	}
 
+	// 3. Now that we have the topologically sorted applicationMetadataNodes
+	// 		create Application instances with dependency links.
 	mApplications := make(map[string]*Application)
 	applications := make(Applications, len(sortedNodes))
 	for i, n := range sortedNodes {
@@ -130,6 +109,8 @@ func (a applicationMetadataSet) toApplications(withDependencies bool) (Applicati
 	return calculateVersion(applications, withDependencies), nil
 }
 
+// calculateVersion takes the topologically sorted Applications and
+// initialises their version field.
 func calculateVersion(topSorted Applications, withDependencies bool) Applications {
 	for _, a := range topSorted {
 		if !withDependencies || len(a.Requires()) == 0 {
@@ -148,8 +129,39 @@ func calculateVersion(topSorted Applications, withDependencies bool) Application
 	return topSorted
 }
 
-// discoverMetadata returns an ApplicationMetadataSet prepared from
-// the specified git commit.
+// applicationMetadataNode is an auxiliary type used to build the dependency
+// graph. Ats as an implementation of graph.Node interface (We use graph
+// library for topological sort).
+type applicationMetadataNode struct {
+	metadata *applicationMetadata
+	set      map[string]*applicationMetadata
+}
+
+func newApplicationMetadataNode(metadata *applicationMetadata, set map[string]*applicationMetadata) *applicationMetadataNode {
+	return &applicationMetadataNode{metadata, set}
+}
+
+func (n *applicationMetadataNode) GetID() interface{} {
+	return n.metadata.spec.Name
+}
+
+func (n *applicationMetadataNode) GetChildren() ([]graph.Node, error) {
+	c := []graph.Node{}
+
+	for _, d := range n.metadata.spec.Dependencies {
+		if s, ok := n.set[d]; ok {
+			c = append(c, newApplicationMetadataNode(s, n.set))
+		} else {
+			return nil, fmt.Errorf("dependency not found %s -> %s", n.metadata.spec.Name, d)
+		}
+	}
+
+	return c, nil
+}
+
+// discoverMetadata walks the git tree at a specific commit looking for
+// directories with .mbt.yml file. Returns an applicationMetadataSet representing
+// the applications found.
 func discoverMetadata(repo *git.Repository, commit *git.Commit) (applicationMetadataSet, error) {
 	tree, err := commit.Tree()
 	if err != nil {
