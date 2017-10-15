9a586a4bfc83753f2683bd4384d9549f49389550
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-10-15 16:59:30 +1100

    Tests

--- lib/discover_test.go
+++ lib/discover_test.go
@@ -20,3 +20,16 @@ func TestDependencyLinks(t *testing.T) {
 	assert.Equal(t, m["app-b"], m["app-c"].RequiredBy()[0])
 	assert.Equal(t, m["app-a"], m["app-b"].RequiredBy()[0])
 }
+
+func TestVersionCalculation(t *testing.T) {
+	a := newApplicationMetadata("app-a", "a", &Spec{Name: "app-a", Dependencies: []string{"app-b"}})
+	b := newApplicationMetadata("app-b", "b", &Spec{Name: "app-b"})
+
+	s := applicationMetadataSet{a, b}
+	apps, err := s.toApplications(true)
+	check(t, err)
+	m := apps.indexByName()
+
+	assert.Equal(t, "b", m["app-b"].Version())
+	assert.Equal(t, "da23614e02469a0d7c7bd1bdab5c9c474b1904dc", m["app-a"].Version())
+}
