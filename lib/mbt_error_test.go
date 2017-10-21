700d95c13ac25e6af21f366c2663f8d47114e16d
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-10-21 17:36:24 +1100

    Improved error interface

--- lib/mbt_error_test.go
+++ lib/mbt_error_test.go
@@ -8,16 +8,21 @@ import (
 )
 
 func TestSimpleError(t *testing.T) {
-	e := newError("a", "b", nil)
-	assert.EqualError(t, e, "a: b")
+	e := newError("a", "b")
+	assert.EqualError(t, e, "mbt a: b")
 }
 
 func TestSimpleErrorWithFormat(t *testing.T) {
-	e := newError("a", "b%s", nil, "c")
-	assert.EqualError(t, e, "a: bc")
+	e := newErrorf("a", "b%s", "c")
+	assert.EqualError(t, e, "mbt a: bc")
 }
 
-func TestInnerError(t *testing.T) {
-	e := newError("a", "b", errors.New("c"))
-	assert.EqualError(t, e, "a: b - c")
+func TestWrappedErrorWithMessage(t *testing.T) {
+	e := wrapm("a", errors.New("c"), "b")
+	assert.EqualError(t, e, "mbt a: b - c")
+}
+
+func TestWrappedError(t *testing.T) {
+	e := wrap("a", errors.New("c"))
+	assert.EqualError(t, e, "mbt a: c")
 }
