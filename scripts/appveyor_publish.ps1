68561f785f41c18e7231e239844ca6e127164501
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-12-04 21:25:13 +1100

    Set appveyor publish args

--- scripts/appveyor_publish.ps1
+++ scripts/appveyor_publish.ps1
@@ -8,7 +8,7 @@ $Headers = @{
 }
 
 if ($Env:APPVEYOR_REPO_TAG_NAME -ne $null) {
-  Invoke-WebRequest -Method Put -Infile build/mbt_windows_x86.zip -Headers $Headers -ContentType "multipart/form-data" -Uri "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
+  Invoke-WebRequest -Method Put -Infile build/mbt_windows_x86.zip -Headers $Headers -ContentType "multipart/form-data" -Uri "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip?publish=1"
 } else {
   Write-Host "Publishing skipped - not a tagged build"
 }
