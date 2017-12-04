31f98d5a6e844cab0fe46c25a7b5ef8c83ab9ebb
buddyspike <buddyspike@geeksdiary.com>
2017-12-04 18:34:55 +1100

    Switch to Invoke-WebRequest

--- scripts/appveyor_publish.ps1
+++ scripts/appveyor_publish.ps1
@@ -1,5 +1,5 @@
-if ($Env:APPVEYOR_REPO_TAG -eq $true) {
-  & curl -T build/mbt_windows_x86.zip -u "buddyspike:$($Env:BINTRAY_APIKEY)" "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
+if ($Env:APPVEYOR_REPO_TAG_NAME -ne $null) {
+  Invoke-WebRequest -Method Put -Infile build/mbt_windows_x86.zip -Credential "buddyspike:$($Env:BINTRAY_APIKEY)" -ContentType "multipart/form-data" -Uri "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
 } else {
   Write-Host "Publishing skipped - not a tagged build"
 }
