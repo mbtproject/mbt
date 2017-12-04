3881e4a2532fabb3aea180f12717d6460184a798
buddyspike <buddyspike@geeksdiary.com>
2017-12-04 18:43:38 +1100

    Fixed IWR code with creds

--- scripts/appveyor_publish.ps1
+++ scripts/appveyor_publish.ps1
@@ -1,5 +1,14 @@
+$credentials = "buddyspike:$($Env:BINTRAY_APIKEY)"
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
 if ($Env:APPVEYOR_REPO_TAG_NAME -ne $null) {
-  Invoke-WebRequest -Method Put -Infile build/mbt_windows_x86.zip -Credential "buddyspike:$($Env:BINTRAY_APIKEY)" -ContentType "multipart/form-data" -Uri "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
+  Invoke-WebRequest -Method Put -Infile build/mbt_windows_x86.zip -Headers $Headers -ContentType "multipart/form-data" -Uri "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
 } else {
   Write-Host "Publishing skipped - not a tagged build"
 }
