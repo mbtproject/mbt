a1cbd8ae561a816510fee0fcdf7f1085ab4528e5
buddyspike <buddyspike@geeksdiary.com>
2017-12-04 18:25:36 +1100

    Trying to find curl in appveyor

--- scripts/appveyor_publish.ps1
+++ scripts/appveyor_publish.ps1
@@ -1,5 +1,5 @@
 if ($Env:APPVEYOR_REPO_TAG -eq $true) {
-  curl -T build/mbt_windows_x86.zip -u "buddyspike:$($Env:BINTRAY_APIKEY)" "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
+  & curl -T build/mbt_windows_x86.zip -u "buddyspike:$($Env:BINTRAY_APIKEY)" "https://api.bintray.com/content/buddyspike/bin/mbt_windows_x86/$($Env:APPVEYOR_REPO_TAG_NAME)/mbt_windows_x86.zip"
 } else {
   Write-Host "Publishing skipped - not a tagged build"
 }
