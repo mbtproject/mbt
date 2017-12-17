5ed65d2ecca33f5025b1f8744e7d8f8ed89b5d6f
buddyspike <buddyspike@geeksdiary.com>
2017-12-17 20:35:05 +1100

    Bump version

--- cmd/version.go
+++ cmd/version.go
@@ -10,6 +10,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Displays the version of mbt",
 	Run: func(cmd *cobra.Command, args []string) {
-		println("0.1.26")
+		println("0.1.27")
 	},
 }
