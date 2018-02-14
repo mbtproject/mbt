09533fc545af2d8f95e618bc2c45e08c5a200310
buddyspike <buddyspike@geeksdiary.com>
2018-02-15 06:07:19 +1100

    Build: Run gofmt and go tool vet as part of build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# Utility functions
+lint() {
+  paths=$@
+  for path in "${paths[@]}"
+  do
+    r=$(gofmt -s -d $path)
+    if [ ! -z "$r" ]; then
+      echo "ERROR: Linting failed. Review errors and try running gofmt -s -w $path"
+      echo $r
+      exit 1
+    fi
+  done
+} 
+
 DIR=$(pwd)
 LIBGIT2_PATH=$DIR/vendor/libgit2
 OS=$(uname -s | awk '{print tolower($0)}')
@@ -12,7 +26,7 @@ go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
 
 # Build libgit2
-./scripts/build_libgit2.sh &&
+./scripts/build_libgit2.sh 
 
 # Set environment so to static link libgit2 when building git2go
 export PKG_CONFIG_PATH="$LIBGIT2_PATH/build"
@@ -29,18 +43,27 @@ fi
 
 OUT="mbt_${OS}_${ARCH}"
 
-make restore &&
+make restore 
+
+# Run linter
+lint *.go ./e ./dtrace ./trie ./lib
 
 # Run tests
-go test ./e -v -covermode=count &&
-go test ./trie -v -covermode=count &&
-go test ./lib -v -covermode=count -coverprofile=coverage.out &&
+go test ./e -v -covermode=count 
+go test ./dtrace -v -covermode=count 
+go test ./trie -v -covermode=count 
+go test ./lib -v -covermode=count -coverprofile=coverage.out 
 if [ ! -z $COVERALLS_TOKEN ] && [ -f ./coverage.out ]; then 
   $HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
 fi
 
-go build -o "build/${OUT}" &&
-shasum -a 1 -p "build/${OUT}" | cut -d ' ' -f 1 > "build/${OUT}.sha1" &&
+go build -o "build/${OUT}" 
+shasum -a 1 -p "build/${OUT}" | cut -d ' ' -f 1 > "build/${OUT}.sha1" 
+
+# Run go vet (this should happen after the build)
+go tool vet ./*.go
+go tool vet ./e ./dtrace ./trie ./lib
+
 echo "testing the bin"
 "./build/${OUT}" version
 
