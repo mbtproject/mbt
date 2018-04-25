5066b72d72e7197e7291f6d339969cb7e392f6e0
buddyspike <buddyspike@geeksdiary.com>
2018-04-26 08:04:10 +1000

    Use Apache License 2.0

--- fsutil/fs.go
+++ fsutil/fs.go
@@ -1,7 +1,23 @@
+/*
+Copyright 2018 MBT Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package fsutil
 
 import "os"
 
+// FileExists returns true if specified file exists
 func FileExists(path string) bool {
 	fi, err := os.Stat(path)
 	if err != nil {
@@ -10,6 +26,7 @@ func FileExists(path string) bool {
 	return !fi.IsDir()
 }
 
+// IsDir returns true if the specified path is a directory
 func IsDir(path string) (bool, error) {
 	stat, err := os.Stat(path)
 	if err != nil {
