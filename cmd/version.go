e158c80bb88aa362da65026e234cebd4135fd18f
figuerascarlos <carlosd.figueras@gmail.com>
2020-06-15 19:55:12 +0200

    fix bad version

--- cmd/version.go
+++ cmd/version.go
@@ -29,6 +29,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show mbt version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.23.0")
+		fmt.Println("v0.24")
 	},
 }
