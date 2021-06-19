de06f6b4a33d5ffd9ac1fb6e249a6fd4b780db3d
pratikbalar <pratikbalar123@gmail.com>
2021-06-19 20:08:03 +0530

    style: lint sh with redirect errors to stderr

--- scripts/upload_git.sh
+++ scripts/upload_git.sh
@@ -7,14 +7,14 @@ set -e
 
 # Ensure that the GITHUB_TOKEN secret is included
 if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
+  echo >&2 "Set the GITHUB_TOKEN env variable."
+  exit 1
 fi
 
 # Ensure that there is a pattern specified.
 if [[ -z "$1" ]]; then
-    echo "Missing file (pattern) to upload."
-    exit 1
+  echo >&2 "Missing file (pattern) to upload."
+  exit 1
 fi
 
 #
@@ -27,30 +27,22 @@ fi
 #
 
 # Have we found any artifacts?
-found=
 for file in $*; do
-    if [ -e "$file" ]; then
-        found=1
-    fi
-done
-
-#
-# Abort if missing.
-#
-if [ -z "${found}" ]; then
-
-    echo "*****************************************************************"
-    echo " "
-    echo " Artifacts are missing, and this action no longer invokes the "
-    echo " legacy-build script."
-    echo " "
-    echo " Please see the README.md file for github-action-publish-binaries"
-    echo " which demonstrates how to build AND upload artifacts."
-    echo " "
-    echo "*****************************************************************"
-
+  if [ -e "$file" ]; then
+    cat >&2 <<MISSING
+"*****************************************************************"
+" "
+" Artifacts are missing, and this action no longer invokes the "
+" legacy-build script."
+" "
+" Please see the README.md file for github-action-publish-binaries"
+" which demonstrates how to build AND upload artifacts."
+" "
+"*****************************************************************"
+MISSING
     exit 1
-fi
+  fi
+done
 
 # Prepare the headers for our curl-command.
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
@@ -61,66 +53,66 @@ RELEASE_ID=$(jq --raw-output '.release.id' "$GITHUB_EVENT_PATH")
 # For each matching file..
 for file in $*; do
 
-    echo "Processing file ${file}"
+  echo "Processing file ${file}"
 
-    if [ ! -e "$file" ]; then
-        echo "***************************"
-        echo " file not found - skipping."
-        echo "***************************"
-        continue
-    fi
+  if [ ! -e "$file" ]; then
+    echo "***************************"
+    echo " file not found - skipping."
+    echo "***************************"
+    continue
+  fi
 
-    if [ ! -s "$file" ]; then
-        echo "**************************"
-        echo " file is empty - skipping."
-        echo "**************************"
-        continue
-    fi
+  if [ ! -s "$file" ]; then
+    echo "**************************"
+    echo " file is empty - skipping."
+    echo "**************************"
+    continue
+  fi
 
-    FILENAME=$(basename "${file}")
+  FILENAME=$(basename "${file}")
 
-    UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
-    echo "Upload URL is ${UPLOAD_URL}"
+  UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
+  echo "Upload URL is ${UPLOAD_URL}"
 
-    # Generate a temporary file.
-    tmp=$(mktemp)
+  # Generate a temporary file.
+  tmp=$(mktemp)
 
-    # Upload the artifact - capturing HTTP response-code in our output file.
-    response=$(curl \
-        -sSL \
-        -XPOST \
-        -H "${AUTH_HEADER}" \
-        --upload-file "${file}" \
-        --header "Content-Type:application/octet-stream" \
-        --write-out "%{http_code}" \
-        --output $tmp \
-        "${UPLOAD_URL}")
+  # Upload the artifact - capturing HTTP response-code in our output file.
+  response=$(curl \
+    -sSL \
+    -XPOST \
+    -H "${AUTH_HEADER}" \
+    --upload-file "${file}" \
+    --header "Content-Type:application/octet-stream" \
+    --write-out "%{http_code}" \
+    --output $tmp \
+    "${UPLOAD_URL}")
 
-    # If the curl-command returned a non-zero response we must abort
-    if [ "$?" -ne 0 ]; then
-        echo "**********************************"
-        echo " curl command did not return zero."
-        echo " Aborting"
-        echo "**********************************"
-        cat $tmp
-        rm $tmp
-        exit 1
-    fi
-
-    # If upload is not successful, we must abort
-    if [ $response -ge 400 ]; then
-        echo "***************************"
-        echo " upload was not successful."
-        echo " Aborting"
-        echo " HTTP status is $response"
-        echo "**********************************"
-        cat $tmp
-        rm $tmp
-        exit 1
-    fi
-
-    # Show pretty output, since we already have jq
-    cat $tmp | jq .
+  # If the curl-command returned a non-zero response we must abort
+  if [ "$?" -ne 0 ]; then
+    echo "**********************************"
+    echo " curl command did not return zero."
+    echo " Aborting"
+    echo "**********************************"
+    cat $tmp
     rm $tmp
+    exit 1
+  fi
+
+  # If upload is not successful, we must abort
+  if [ $response -ge 400 ]; then
+    echo "***************************"
+    echo " upload was not successful."
+    echo " Aborting"
+    echo " HTTP status is $response"
+    echo "**********************************"
+    cat $tmp
+    rm $tmp
+    exit 1
+  fi
+
+  # Show pretty output, since we already have jq
+  cat $tmp | jq .
+  rm $tmp
 
 done
